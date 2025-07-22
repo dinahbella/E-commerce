b94796f6e3ed04b71179c30629ef2b35c76e1e7b
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-07-22 17:07:53 +0100

    userModel

---
+++ backend/model/userModel.js
@@ -0,0 +1,53 @@
+import e from "express";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+      trim: true,
+      maxlength: [50, "Name cannot exceed 30 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      unique: true,
+      trim: true,
+      maxlength: [50, "Email cannot exceed 50 characters"],
+      validate: {
+        validator: function (value) {
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          return emailRegex.test(value);
+        },
+        message: "Please enter a valid email address",
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      minlength: [6, "Password must be at least 6 characters"],
+      select: false, // Do not return password in queries
+    },
+    avatar: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+export default mongoose.model("User", userSchema);
