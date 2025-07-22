5555f61d912093fca1e2c4f44c5c65cef32b26dd
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-07-22 11:02:33 +0100

    index.js

--- backend/index.js
+++ backend/index.js
@@ -6,5 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log("Server is running on port 3000");
+  console.log(
+    `Server is running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`
+  );
 });
