c1641c7082c4cc87d7ad080869c535110075b4e6
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-07-23 12:31:17 +0100

    orderModel

---
+++ backend/model/orderModel.js
@@ -0,0 +1,107 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    shippingInfo: {
+      address: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+      zipCode: {
+        type: String,
+        required: true,
+      },
+      phoneNo: {
+        type: String,
+        required: true,
+      },
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    orderItems: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        image: {
+          type: String,
+          required: true,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+      },
+    ],
+    paymentMethod: {
+      type: String,
+      required: [true, , "Please select a payment method"],
+      enum: {
+        values: ["Credit Card", "Cash on Delivery"],
+        message: "Please select a valid payment method",
+      },
+    },
+    paymentInfo: {
+      id: {
+        type: String,
+      },
+      status: {
+        type: String,
+      },
+    },
+    itemPrice: {
+      type: Number,
+      required: true,
+    },
+    taxAmount: {
+      type: Number,
+      required: true,
+    },
+    shippingAmount: {
+      type: Number,
+      required: true,
+    },
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    orderStatus: {
+      type: String,
+      required: true,
+      default: "Processing",
+      enum: {
+        values: ["Processing", "Shipped", "Delivered", "Cancelled"],
+        message: "Please select a valid order status",
+      },
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
